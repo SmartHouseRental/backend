727f5e28a89e6ba856ecc2c35e3a1e0909b24503
nebutty <nebathussen2@gmail.com>
2026-05-14 14:55:09 +0300

    issues fixed

--- src/app.ts
+++ src/app.ts
@@ -48,7 +48,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true, // Allow cookies
   })
 );
